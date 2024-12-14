818a2f8489f1b2a22e78ba9f02167de5c93dc2fa
daseet <wcyd@duck.com>
2024-12-14 14:21:11 +1100

    GMMK Pro Rev1 ANSI custom keymap
    - Add RGB control
    - Disable LED for all keys except the sides

--- keyboards/gmmk/pro/rev1/ansi/keymaps/andy/keymap.c
+++ keyboards/gmmk/pro/rev1/ansi/keymaps/andy/keymap.c
@@ -15,19 +15,19 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include QMK_KEYBOARD_H
+#include "rgb_matrix_map.h"
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-//      ESC      F1       F2       F3       F4       F5       F6       F7       F8       F9       F10      F11      F12	     Prt           Rotary(Mute)
-//      ~        1        2        3        4        5        6        7        8        9        0         -       (=)	     BackSpc           Del
-//      Tab      Q        W        E        R        T        Y        U        I        O        P        [        ]        \                 PgUp
-//      Caps     A        S        D        F        G        H        J        K        L        ;        "                 Enter             PgDn
-//      Sh_L              Z        X        C        V        B        N        M        ,        .        ?                 Sh_R     Up       End
-//      Ct_L     Win_L    Alt_L                               SPACE                               Alt_R    FN       Ct_R     Left     Down     Right
+//      ESC             F1        F2       F3       F4        F5        F6       F7       F8       F9       F10      F11      F12	   Prt                  Rotary(Mute)
+//      ~               1         2        3        4         5         6        7        8        9        0         -       (=)	   BackSpc              Del
+//      Tab             Q         W        E        R         T         Y        U        I        O        P        [        ]        \                    PgUp
+//      Caps            A         S        D        F         G         H        J        K        L        ;        "                 Enter                PgDn
+//      Sh_L            Z         X        C        V         B         N        M        ,        .        ?                 Sh_R     Up                   End
+//      Ct_L            Win_L     Alt_L                                 SPACE                      Alt_R    FN       Ct_R     Left     Down     Right
 
-
-    // The FN key by default maps to a momentary toggle to layer 1 to provide access to the QK_BOOT key (to put the board into bootloader mode). Without
+    // The FN key by default maps to a momentary toggle to layer 1 to provide access to the RESET key (to put the board into bootloader mode). Without
     // this mapping, you have to open the case to hit the button on the bottom of the PCB (near the USB cable attachment) while plugging in the USB
     // cable to get the board into bootloader mode - definitely not fun when you're working on your QMK builds. Remove this and put it back to KC_RGUI
     // if that's your preference.
@@ -38,7 +38,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // This keyboard defaults to 6KRO instead of NKRO for compatibility reasons (some KVMs and BIOSes are incompatible with NKRO).
     // Since this is, among other things, a "gaming" keyboard, a key combination to enable NKRO on the fly is provided for convenience.
     // Press Fn+N to toggle between 6KRO and NKRO. This setting is persisted to the EEPROM and thus persists between restarts.
-
     [0] = LAYOUT(
         KC_ESC,  		KC_F1,     KC_F2,   KC_F3,   KC_F4,    KC_F5,    KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,    			KC_MUTE,
         KC_GRV,  		KC_1,      KC_2,    KC_3,    KC_4,     KC_5,     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,          	KC_HOME,
@@ -66,12 +65,58 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,        _______,   _______,                              KC_MPLY,                   _______, _______, _______, _______, _______, _______
     )
 };
-// clang-format on
 
-#if defined(ENCODER_MAP_ENABLE)
+// Encoder
+#ifdef ENCODER_MAP_ENABLE
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
     [0] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
-    [1] = { ENCODER_CCW_CW(KC_TRNS, KC_TRNS) },
-    [2] = { ENCODER_CCW_CW(KC_TRNS, KC_TRNS) }
+    [1] = { ENCODER_CCW_CW(RGB_HUD, RGB_HUI) },
+    [2] = { ENCODER_CCW_CW(KC_F23, KC_F24) }
 };
+#endif
+
+// clang-format on
+
+#ifdef RGB_MATRIX_ENABLE
+bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
+	// Turn off side LED if caps lock is enabled
+	if (host_keyboard_led_state().caps_lock) {
+        for (uint8_t i=0; i<sizeof(LED_SIDE_LEFT)/sizeof(LED_SIDE_LEFT[0]); i++) {
+                rgb_matrix_set_color(LED_SIDE_LEFT[i], RGB_OFF);
+            }
+		for (uint8_t i=0; i<sizeof(LED_SIDE_RIGHT)/sizeof(LED_SIDE_RIGHT[0]); i++) {
+                rgb_matrix_set_color(LED_SIDE_RIGHT[i], RGB_OFF);
+            }
+    }
+
+    switch(get_highest_layer(layer_state)){
+        // Set side LED to white while caps lock is held
+        case 2:
+        for (uint8_t i=0; i<sizeof(LED_SIDE_LEFT)/sizeof(LED_SIDE_LEFT[0]); i++) {
+                rgb_matrix_set_color(LED_SIDE_LEFT[i], RGB_WHITE);
+            }
+		for (uint8_t i=0; i<sizeof(LED_SIDE_RIGHT)/sizeof(LED_SIDE_RIGHT[0]); i++) {
+                rgb_matrix_set_color(LED_SIDE_RIGHT[i], RGB_WHITE);
+            }
+        break;
+        // Turn off all key LED by default
+        default:
+            for (uint8_t i=0; i<sizeof(LED_KEYS)/sizeof(LED_KEYS[0]); i++) {
+                    rgb_matrix_set_color(LED_KEYS[i], RGB_OFF);
+                }
+            break;
+        break;
+    };
+    return false;
+};
+
+
+void suspend_power_down_user(void) {
+        rgb_matrix_set_suspend_state(true);
+    };
+
+void suspend_wakeup_init_user(void) {
+        rgb_matrix_set_suspend_state(false);
+    };
+
 #endif
